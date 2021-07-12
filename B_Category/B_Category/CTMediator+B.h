8c91db408358af796d86e3bdb184261b2ac153f8
zpr <2337262844@qq.com>
2021-07-12 10:59:05 +0800

    no message

---
+++ B_Category/B_Category/CTMediator+B.h
@@ -0,0 +1,18 @@
+//
+//  CTMediator+B.h
+//  B_Category
+//
+//  Created by 朱浦睿 on 2021/7/12.
+//
+
+#import <CTMediator/CTMediator.h>
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CTMediator (B)
+- (UIViewController *)B_viewControllerWithContentText:(NSString *)contentText;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ B_Category/B_Category/CTMediator+B.m
@@ -0,0 +1,22 @@
+//
+//  CTMediator+B.m
+//  B_Category
+//
+//  Created by 朱浦睿 on 2021/7/12.
+//
+
+#import "CTMediator+B.h"
+
+@implementation CTMediator (B)
+
+
+- (UIViewController *)B_viewControllerWithContentText:(NSString *)contentText
+{
+    /*
+        BViewController *viewController = [[BViewController alloc] initWithContentText:@"hello, world!"];
+     */
+    NSMutableDictionary *params = [[NSMutableDictionary alloc] init];
+    params[@"contentText"] = contentText;
+    return [self performTarget:@"B" action:@"viewController" params:params shouldCacheTarget:NO];
+}
+@end
